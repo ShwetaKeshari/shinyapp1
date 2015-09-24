6bf7b22ea94c58a8a86a8586949bd2ede1290623
ShwetaKeshari <swetakeshari@gmail.com>
2015-09-24 09:45:06 -0500

    final project on shiny app

--- server.R
+++ server.R
@@ -1,65 +1,36 @@
-
-
 library(shiny)
 
 # Data about the Emissions over a period from 1999 to 2008 for different counties
 
 # Create a data frame from the data extracted
 
-#a<-getURL("https://docs.google.com/spreadsheets/d/1HVtOusW48PJ-P-cD8LlkmztmY6CUJ2_2ApCvIumIBpk/pub?gid=0&single=true&output=csv")
-#d<-read.csv(textConnection(a))
-
-loadGoogSheet <- function(URL, key = NULL,
-                          stringsAsFactors = default.stringsAsFactors(), na.strings = "NA",
-                          colClasses = NA, blank.lines.skip = TRUE
-) 
-{
-  
-  #.try_require("RCurl")
-  library(googlesheets)
-  require("RCurl")
-  if (missing(URL) & !is.null(key)) 
-    URL = paste(https://docs.google.com/spreadsheets/d/1HVtOusW48PJ-P-cD8LlkmztmY6CUJ2_2ApCvIumIBpk/pub?gid=0&single=true&output=csv",key, "&single=TRUE&gid=0", "&output=csv", sep = "")
-  s = RCurl::getURLContent(URL)
-  foo = textConnection(s)
-  b = read.csv(foo,
-               stringsAsFactors = stringsAsFactors,
-               na.strings = na.strings,
-               colClasses = colClasses,
-               blank.lines.skip = blank.lines.skip
-  )
-  close(foo)
-  return(b)
-  
-}
-
-
-#d<-read.csv(emission,sep=",",colnames=c("Baltimore","Los Angeles"))
 
 # Define a server for the Shiny app
 shinyServer(function(input, output) {
- 
   
- 
+  
+  
   
   # Make the bar plot for the total Emission from Baltimore City and Los Angeles city from 1999 to 2008.
   output$Emissions <- renderPlot({
     
-    
-   # a<-c(47103.19,26968.79,22939.78,32135.48)
-    #b<-c(3274.180,2453.916,3091.354,1862.282)
-    #d<-data.frame(a,b)
-    #names(d)<-c("Baltimore","Los Angeles")
-    
-    
-    
-     barplot(b[,input$region],
-            
-          main=input$region,
-          col="red",
-           ylab="Total Amount of PM2.5 emitted, in tons",
-           xlab="Year")
-    axis(1,c(1:4),c(1999,2002,2004,2008))
-    })
-})
   
+    
+    if (input$region=="Baltimore"){barplot(height = c(47103.19,26968.79,22939.78,32135.48),
+                                           
+                                           main=input$region,
+                                           col="red",
+                                           ylab="Total Amount of PM2.5 emitted, in tons",
+                                           xlab="Year")
+      axis(1,c(1:4),c(1999,2002,2004,2008))}
+    else if 
+    (input$region=="Los Angeles"){barplot(height = c(3274.180,2453.916,3091.354,1862.282),
+                                        
+                                        main=input$region,
+                                        col="red",
+                                        ylab="Total Amount of PM2.5 emitted, in tons",
+                                        xlab="Year")
+      axis(1,c(1:4),c(1999,2002,2004,2008))}
+    
+  })
+})
--- ui.R
+++ ui.R
@@ -16,20 +16,31 @@ shinyUI(
     titlePanel(h3("Comparison of Total Emission from PM2.5 by City from 1999 to 2008")),
     
     # Generate a row with a sidebar
-    sidebarLayout(      
+    sidebarLayout(   
       
       # Define the sidebar with one input
       sidebarPanel(
-        selectInput("region", "City:", 
-                    choices=colnames(d))# choose between two city whose total emission over time we would like to see.
+        selectInput("region", "Choose the City:", 
+                    choices=c("Baltimore","Los Angeles"))# choose between two city whose total emission over time we would like to see.
         
-    
+        
       ),
       
       # Create a spot for the barplot
-      mainPanel("Total Emission from PM2.5 from:",
-        plotOutput("Emissions"),"The overall total emisssion from Baltimore City and Los Angeles city has decreased from 1990 to 2008 (over 10 year period) " 
-      )
+      mainPanel("Have total emissions from PM2.5 decreased in the Baltimore City, Maryland and Los Angeles from 1999 to 2008? " ,
+                "The overall total emisssion from Baltimore City and Los Angeles city has decreased from 1990 to 2008 (over 10 year period) ",
+                
+                
+                
+                 plotOutput("Emissions"), 
+                
+                
+                "Note: Fine particulate matter (PM2.5) is an ambient air pollutant for which there is strong evidence that it is harmful to human health. In the United States, the Environmental Protection Agency (EPA) is tasked with setting national ambient air quality standards for fine PM and for tracking the emissions of this pollutant into the atmosphere. Approximatly every 3 years, the EPA releases its database on emissions of PM2.5."
+      
+                
+                
+                
+                )
       
     )
   )
